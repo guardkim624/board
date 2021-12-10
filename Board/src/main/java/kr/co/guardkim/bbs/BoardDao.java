69a06474ff8f635114da7ac763d2e7dc92cbcd5c
guardkim <guardkim624@gmail.com>
2021-12-10 16:38:01 +0900

    게시판 글쓰기 성공

---
+++ Board/src/main/java/kr/co/guardkim/bbs/BoardDao.java
@@ -0,0 +1,76 @@
+package kr.co.guardkim.bbs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class BoardDao {
+
+	private Connection conn;
+	private ResultSet rs;
+	
+	//기본 생성자
+	public BoardDao() {
+		try {
+			String dbURL = "jdbc:oracle:thin:@localhost:1521:xe";
+			String dbID = "board";
+			String dbPassword = "java";
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	//작성일자 메소드
+	public String getDate() {
+		String sql = "select now()";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getString(1);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ""; //데이터베이스 오류
+	}
+	
+	//게시글 번호 부여 메소드
+	public int getNext() {
+		//현재 게시글을 내림차순으로 조회하여 가장 마지막 글의 번호를 구한다
+		String sql = "select boardId from bbs order by boardId desc";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getInt(1) + 1;
+			}
+			return 1; //첫 번째 게시물인 경우
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+	
+	//글쓰기 메소드
+	public int write(String bbsTitle, String userId, String bbsContent) {
+		String sql = "insert into bbs values(?, ?, ?, ?, ?, ?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, getNext());
+			pstmt.setString(2, bbsTitle);
+			pstmt.setString(3, userId);
+			pstmt.setString(4, getDate());
+			pstmt.setString(5, bbsContent);
+			pstmt.setInt(6, 1); //글의 유효번호
+			return pstmt.executeUpdate();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+	
+}
---
+++ Board/src/main/java/kr/co/guardkim/bbs/BoardVo.java
@@ -0,0 +1,49 @@
+package kr.co.guardkim.bbs;
+
+public class BoardVo {
+	
+	private int boardId;
+	private String boardTitle;
+	private String userId;
+	private String boardDate;
+	private String boardContent;
+	private int boardAvailable;
+	
+	public int getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getBoardDate() {
+		return boardDate;
+	}
+	public void setBoardDate(String boardDate) {
+		this.boardDate = boardDate;
+	}
+	public String getBoardContent() {
+		return boardContent;
+	}
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+	public int getBoardAvailable() {
+		return boardAvailable;
+	}
+	public void setBoardAvailable(int boardAvailable) {
+		this.boardAvailable = boardAvailable;
+	}
+	
+}
